608c584f47b29bcffc7318eb1453e957fcf80083
edwardsbean <edwardsbean@gmail.com>
2018-07-25 14:43:35 +0800

    助手函数db默认不创建新的链接

--- src/edphp/helper.php
+++ src/edphp/helper.php
@@ -64,10 +64,10 @@ if (!function_exists('db')) {
      * 实例化数据库类
      * @param string        $name 操作的数据表名称（不含前缀）
      * @param array|string  $config 数据库配置参数
-     * @param bool          $force 是否强制重新连接
+     * @param bool          $force 是否强制使用新的链接 
      * @return \edphp\db\Query
      */
-    function db($name = '', $config = [], $force = true)
+    function db($name = '', $config = [], $force = false)
     {
         return Db::connect($config, $force)->name($name);
     }
