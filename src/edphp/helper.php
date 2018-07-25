04c6e28043314df7fe5293a4373e810a79f388f1
edwardsbean <edwardsbean@gmail.com>
2018-07-25 13:25:09 +0800

    add upload file

--- src/edphp/helper.php
+++ src/edphp/helper.php
@@ -328,6 +328,17 @@ if (!function_exists('token')) {
     }
 }
 
+if (!function_exists('request')) {
+    /**
+     * 获取当前Request对象实例
+     * @return Request
+     */
+    function request()
+    {
+        return Request::getInstance();
+    }
+}
+
 if (!function_exists('session')) {
     /**
      * Session管理
